0cf2557bcea114fd50654d3f0635c8ba63197a37
Alperen Yurdakul <alperenyurdakul7@gmail.com>
2020-02-06 19:22:23 +0100

    Couple of fixes

--- Source.cpp
+++ Source.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* argv[])
 	("An Test SDL2 window", // window's title
 		0, 0, // coordinates on the screen, in pixels, of the window's upper left corner
 		1920, 1080, // window's length and height in pixels  
-		SDL_WINDOW_FULLSCREEN);
+		SDL_WINDOW_FULLSCREEN_DESKTOP);
 
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ABGR8888, SDL_TEXTUREACCESS_STATIC, 1920, 1080);
